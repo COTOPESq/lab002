42c180308e743f2c358a61a238e2afeab1b11655
COTOPESq <slava20082005@gmail.com>
2024-04-15 19:07:20 +0300

    clang-format

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include <string> 
+#include <string>
 
-int main(){
-string name;
-std::cout << "enter name: " << std::endl; //show text
-std::cin >> name; //enter name
-std::cout << "Hello world " << name << std::endl; //result
+int
+main()
+{
+  string name;
+  std::cout << "enter name: " << std::endl;         // show text
+  std::cin >> name;                                 // enter name
+  std::cout << "Hello world " << name << std::endl; // result
 }
-

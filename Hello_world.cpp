d912ac7eaf44fedfe7617233bcacdf4ef24bcf07
COTOPESq <slava20082005@gmail.com>
2024-04-15 18:57:32 +0300

    add commetary

--- Hello_world.cpp
+++ Hello_world.cpp
@@ -3,8 +3,8 @@
 
 int main(){
 string name;
-std::cout << "enter name: " << std::endl;
-std::cin >> name;
-std::cout << "Hello world " << name << std::endl;
+std::cout << "enter name: " << std::endl; //show text
+std::cin >> name; //enter name
+std::cout << "Hello world " << name << std::endl; //result
 }
 
